098c83d50e8cd975570ec61a9f99886781a31ab2
Tevin Thuku <tevinthuku@gmail.com>
2019-06-14 08:05:18 +0300

    - write command to fetch single article - rectify command to fetch all articles.

--- cli.js
+++ cli.js
@@ -1,10 +1,15 @@
-const { getAllArticles } = require("./src/fetch");
+const { getAllArticles, getSingleArticle } = require("./src/fetch");
 
 const stats = (function(args) {
-  switch (args.join()) {
-    case "--list":
+  const args_string = args.join(" ");
+  switch (true) {
+    case /^ah list$/.test(args_string):
       console.log("⏱️  ⏱️  Kindly wait as I fetch the articles available.");
       return getAllArticles();
+    case /ah view/.test(args_string):
+      console.log("⏱️  ⏱️ Kindly wait as we get your desired article.");
+      getSingleArticle(args);
+      return;
     default:
       console.log("Command not found");
   }
